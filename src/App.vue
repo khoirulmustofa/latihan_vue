0ab1960b048f2c6598d52de174ca68b5d944e5f6
Khoirul Mustofa <khoirul.mustofa2014@gmail.com>
2022-12-17 06:34:17 +0700

    4 V-Model

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,21 @@
   <h3>Declarative Rendering</h3>
   <div>{{ messege }}</div>
   <button :title="tooltip">Hover Here</button>
-
+  <!-- ======================================= -->
   <hr>
 
   <h3>Methods</h3>
   <p>{{ count }}</p>
   <button @click="count++">Klik Tambah</button>
   <button @click="addDouble">Tambah Double</button>
+
+  <!-- ============================================== -->
+  <hr>
+  <h3>V-Model</h3>
+  <p style="color: red;">{{ inputText }}</p>
+  <input v-model="inputText" />
+
+  <input type="text" :value="inputText" @input="inputText = $event.target.inputText" />
 
 </template>
 <script>
@@ -18,11 +26,12 @@ export default {
     return {
       messege: 'Hallo Vue',
       tooltip: 'This Tooltip',
-      count: 0
+      count: 0,
+      inputText: ""
     }
   },
-  methods:{
-    addDouble(){
+  methods: {
+    addDouble() {
       this.count = this.count + 5;
     }
   }
