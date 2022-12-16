e3b39af178386702e7b1db74e2afd756ff097ee6
Khoirul Mustofa <khoirul.mustofa2014@gmail.com>
2022-12-17 06:17:49 +0700

    #3: Methods

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,37 @@
 <template>
-  <div>{{messege}}</div>
+  <h3>Declarative Rendering</h3>
+  <div>{{ messege }}</div>
   <button :title="tooltip">Hover Here</button>
+
+  <hr>
+
+  <h3>Methods</h3>
+  <p>{{ count }}</p>
+  <button @click="count++">Klik Tambah</button>
+  <button @click="addDouble">Tambah Double</button>
+
 </template>
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  data(){
-    return{
+  data() {
+    return {
       messege: 'Hallo Vue',
-      tooltip: 'This Tooltip'
+      tooltip: 'This Tooltip',
+      count: 0
+    }
+  },
+  methods:{
+    addDouble(){
+      this.count = this.count + 5;
     }
   }
+}
   // name: 'App',
   // components: {
   //   HelloWorld
   // }
 
-}</script>
+</script>
 
 
